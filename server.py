a341f5f2fb5624cf71dfbcadeddb1e575e272510
x9nu <light_blue_dre@163.com>
2023-12-31 12:45:15 +0800

    ver1.0

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import gc
 import os
+import shutil
 import time
 import uuid
-from datetime import datetime, timedelta
+from threading import Lock
+
 import torch
 from flask import Flask, session, request, send_from_directory
 from flask_apscheduler import APScheduler
@@ -21,26 +23,29 @@ app.secret_key = os.urandom(24)
 
 # 初始化checkpoint最后使用时间
 last_access_time = time.time()
+checkpoint_lock = Lock()
 checkpoint = None
 
 
 def clear_checkpoint():
     global last_access_time, checkpoint
     # 每10分钟检查，如果checkpoint闲置时间在10分钟以上将卸载它.闲置时间必须大于每次处理掩码的时间。最好设置高一点，不然在使用途中也可能被卸载。
-    if time.time()- last_access_time > 60*10:
-        checkpoint = None
-        # 如果被载入到GPU专用内存，则需要使用torch来清除缓存。
-        torch.cuda.empty_cache()
-        # 需要显式地回收
-        gc.collect()
-        print("Checkpoint cleared due to inactivity.")
+    with checkpoint_lock:
+        if time.time() - last_access_time > 60 * 10:
+            checkpoint = None
+            # 如果被载入到GPU专用内存，则需要使用torch来清除缓存。
+            torch.cuda.empty_cache()
+            # 需要显式地回收
+            gc.collect()
+            print("Checkpoint cleared due to inactivity.")
 
 
 def load_checkpoint_if_needed():
     global checkpoint
-    if checkpoint is None:
-        # 载入checkpoint
-        checkpoint = CHECKPOINT
+    with checkpoint_lock:
+        if checkpoint is None:
+            # 载入checkpoint
+            checkpoint = CHECKPOINT
 
 
 def clear_temp():
@@ -57,7 +62,7 @@ def clear_temp():
             print(f"Deleted old file: {filename}")
 
 
-# 每30分钟清理一次临时文件，30分钟检查checkpoint是否闲置
+# 每60分钟清理一次临时文件，2分钟检查checkpoint是否闲置在10分钟以上（闲置时间10~12分钟）
 app.config['JOBS'] = [
     {
         'id': 'clear_temp',
@@ -69,7 +74,7 @@ app.config['JOBS'] = [
         'id': 'clear_checkpoint',
         'func': clear_checkpoint,
         'trigger': 'interval',
-        'minutes': 2,# 十分钟检查一次
+        'minutes': 2,  # 十分钟检查一次
     }
 ]
 
@@ -81,40 +86,44 @@ scheduler.start()
 # 上传图片生成掩码
 @app.route('/upload_image', methods=['POST'])
 def upload_image():
-    global last_access_time
-    # 更新最后使用时间，用它来检查一段时间如果没有使用模型，就回收checkpoint
-    last_access_time = time.time()
-    # 确保checkpoints载入
+    # 确保checkpoints载入，因为这个函数内已经带checkpoint_lock锁了，不能再带一次。
     load_checkpoint_if_needed()
-    unique_id = uuid.uuid4()
-    session['unique_id'] = str(unique_id)
-    file_name = f"{unique_id}.png"
+    # 需要带锁
+    with checkpoint_lock:
+        global last_access_time
+        # 更新使用时间，用它来检查一段时间如果没有使用模型，就回收checkpoint
+        last_access_time = time.time()
 
-    if 'file' not in request.files:
-        return "No file part", 400
-    file = request.files['file']
-    if file.filename == '':
-        return "No selected file", 400
-    # 验证文件是否为图像
-    if not allowed_file(file.filename):
-        return "Invalid file type", 415  # 415 不支持的媒体格式（要求是图片）
+        unique_id = uuid.uuid4()
+        session['unique_id'] = str(unique_id)
+        file_name = f"{unique_id}.png"
 
-    if file:
-        save_path = os.path.join(app.config['UPLOAD'], file_name)
-        file.save(save_path)
-        input_image_path = f'./upload/{file_name}'
-        masks_res = f'./upload/{unique_id}.txt'
+        if 'file' not in request.files:
+            return "No file part", 400
+        file = request.files['file']
+        if file.filename == '':
+            return "No selected file", 400
+        # 验证文件是否为图像
+        if not allowed_file(file.filename):
+            return "Invalid file type", 415  # 415 不支持的媒体格式（要求是图片）
 
-        num_masks = generate_masks(input_image_path, checkpoint, masks_res)
+        if file:
+            save_path = os.path.join(app.config['UPLOAD'], file_name)
+            file.save(save_path)
+            input_image_path = f'./upload/{file_name}'
+            masks_res = f'./upload/{unique_id}.txt'
 
-        print(f"Generated {num_masks} masks.")
+            num_masks = generate_masks(input_image_path, checkpoint, masks_res)
 
-        del input_image_path, masks_res
-        gc.collect()
+            print(f"Generated {num_masks} masks.")
 
-        return f"{num_masks} Masks generated and saved", 200
-    else:
-        return "Error in processing", 500
+            del input_image_path, masks_res
+            gc.collect()
+            # 用完也要更新checkpoint使用时间
+            last_access_time = time.time()
+            return f"{num_masks} Masks generated and saved", 200
+        else:
+            return "Error in processing", 500
 
 
 # 根据session获取掩码
@@ -151,6 +160,10 @@ def index():
 
 
 if __name__ == '__main__':
+    # 如果 UPLOAD 目录存在，删除它及其所有内容
+    if os.path.exists(UPLOAD):
+        shutil.rmtree(UPLOAD)
+    # 重新创建
     if not os.path.exists(UPLOAD):
         os.makedirs(UPLOAD)
     app.run(host='0.0.0.0', port=5000)
